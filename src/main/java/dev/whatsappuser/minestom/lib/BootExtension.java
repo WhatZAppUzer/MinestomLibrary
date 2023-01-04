2ce657148c09f1daf07db4b57488a9831c983166
Regno-WhatsAppUser <88800877+Regno-WhatsAppUser@users.noreply.github.com>
2023-01-04 17:00:08 +0100

    getter for PermissionProvider in main class

--- src/main/java/dev/whatsappuser/minestom/lib/BootExtension.java
+++ src/main/java/dev/whatsappuser/minestom/lib/BootExtension.java
@@ -1,12 +1,10 @@
 package dev.whatsappuser.minestom.lib;
 
 import dev.whatsappuser.minestom.lib.permissions.PermissionProvider;
-import lombok.Getter;
 
 /**
  * development by TimoH created on 16:04:12 | 04.01.2023
  */
-@Getter
 public abstract class BootExtension extends Extension {
 
     private static PermissionProvider provider;
@@ -14,5 +12,9 @@ public abstract class BootExtension extends Extension {
     @Override
     public void preInitialize() {
         provider = new PermissionProvider(getOrigin().getName());
+    }
+
+    public static PermissionProvider provider() {
+        return provider;
     }
 }
